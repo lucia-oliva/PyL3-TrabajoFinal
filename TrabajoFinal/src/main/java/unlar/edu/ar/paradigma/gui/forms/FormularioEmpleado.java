4d6f0691f5e3992f15e8ce040bc3809e04d72a12
Lucia Oliva <lucia222lr@gmail.com>
2024-11-09 16:18:44 -0300

    adding GenericAddForm
    Formulario que se abre cuando clickeas sobre el boton "agregar" y adapta los labels segun la tabla donde nos encontemos...  (SOLO ESTA APLICADO A BOTON AGREGAR DE FORM EMPLEADOS, hay que aplicarlo al resto de entidades y al principal.)

--- TrabajoFinal/src/main/java/unlar/edu/ar/paradigma/gui/forms/FormularioEmpleado.java
+++ TrabajoFinal/src/main/java/unlar/edu/ar/paradigma/gui/forms/FormularioEmpleado.java
@@ -5,6 +5,9 @@
 package unlar.edu.ar.paradigma.gui.forms;
 
 import java.awt.BorderLayout;
+import java.awt.Frame;
+import java.util.List;
+import javax.swing.SwingUtilities;
 import javax.swing.table.DefaultTableModel;
 
 
@@ -104,6 +107,7 @@ public class FormularioEmpleado extends javax.swing.JPanel {
     // End of variables declaration//GEN-END:variables
 
     private void initCustomComponents() {
+        
          DefaultTableModel model = new DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
@@ -120,7 +124,20 @@ public class FormularioEmpleado extends javax.swing.JPanel {
         jScrollPane1.setPreferredSize(new java.awt.Dimension(800, 400)); 
 
          jButton1.setText("Agregar");
-
+         
+          jButton1.addActionListener(e -> {
+           // Definir las columnas que se mostrarán en el formulario agregar
+           List<String> columnasEmpleado = List.of("ID-Legajo", "Apellido-Nombre");
+            
+// Crear y mostrar el formulario de agregar
+            GenericFormAgregar formulario = new GenericFormAgregar(
+                (Frame) SwingUtilities.getWindowAncestor(this), 
+                true, 
+                columnasEmpleado
+            );
+            formulario.setVisible(true);
+          });
+            
         jButton2.setText("Modificar");
 
         jButton3.setText("Eliminar");
